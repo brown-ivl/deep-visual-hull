b642cee089c48b253158eb06f823c2342f78aa64
cranberrymuffin <aparna_natarajan@brown.edu>
2021-07-13 13:18:11 -0400

    stop overfitting

--- run.py
+++ run.py
@@ -100,7 +100,7 @@ if __name__ == "__main__":
     if flags.mode == "train":
         print("TRAIN mode")
 
-        training_data = DvhShapeNetDataset(config.instance_dir, config.resolution, overfitting=True)
+        training_data = DvhShapeNetDataset(config.instance_dir, config.resolution)
         train_dataloader = torch.utils.data.DataLoader(training_data,
                                                        batch_size=config.batch_size)  # shuffle=True, num_workers=4
         model = dvhNet()
@@ -148,7 +148,7 @@ if __name__ == "__main__":
         print("load_ckpt_dir's latest checkpoint filepath:", ckpt_fp)
         model = dvhNet()
         model.load_state_dict(torch.load(ckpt_fp))
-        test_data = DvhShapeNetDataset(config.instance_dir, config.resolution, overfitting=True)
+        test_data = DvhShapeNetDataset(config.instance_dir, config.resolution)
         test_dataloader = torch.utils.data.DataLoader(test_data,
                                                       batch_size=config.batch_size)  # shuffle=True, num_workers=4
         loss_fn = nn.BCELoss()
