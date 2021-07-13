764654ca62976e30c979e22d6e23c1be2d1e65bd
cranberrymuffin <aparna_natarajan@brown.edu>
2021-07-13 17:07:39 -0400

    delete unused dataloader

--- data.py
+++ data.py
@@ -1,40 +1,7 @@
-import os
-import torch
-from torchvision.transforms import ToTensor
-from util import calculate_voxel_centers
-import binvox_rw as binvox
-import cv2
 import pathlib
 import torch
 import glob
 from util import get_image, nocs2voxel, calculate_voxel_centers, img_path2numpy
-
-"""
-class CustomImageDataset(torch.utils.data.Dataset):
-    def __init__(self, instance_dir, resolution, transform=None, target_transform=None):
-        self.instance_dir = instance_dir
-        instance_fp = os.listdir(instance_dir)
-        instance_fp.sort()
-        self.image_paths = [os.path.join(instance_dir, f) for f in instance_fp if 'Color_00' in f]
-        self.voxel_grid_paths = [os.path.join(instance_dir, f) for f in instance_fp if 'voxel_grid' in f]
-        self.transform = transform
-        self.target_transform = target_transform
-        self.points = calculate_voxel_centers(resolution)
-
-    def __len__(self):
-        return len(self.image_paths)
-
-    def __getitem__(self, idx):
-        image = get_image(self.image_paths[idx])
-        with open(self.voxel_grid_paths[idx], "rb") as f:
-            voxels = binvox.read_as_3d_array(f)
-            voxel_grid = voxels.data.astype(float)
-        if self.transform:
-            image = self.transform(image)
-        if self.target_transform:
-            voxel_grid = self.target_transform(voxel_grid)
-        return image[:3, :, :] , self.points.detach().clone(), voxel_grid
-"""
 
 COLOR_IMAGE_FILE_PATH_PATTERN = "*Color_00*"
 NOCS_MAP_FILE_PATH_PATTERN = "*NOX*"
