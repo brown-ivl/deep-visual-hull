4f60d9200258c036d688447d892cc8bed1cdaa49
sjdingcn <sijie_ding@brown.edu>
2021-07-28 16:18:41 -0400

    add: config

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 resolution = 16 # resolution of voxel grid binvox files and voxel centers
 instance_dir = 'car_instance' # "shapenetplain_v1/train/02958343/1a0bc9ab92c915167ae33d942430658c"
 batch_size = 8
-learning_rate = 1e-3
+learning_rate = 1e-4
 save_dir = './saved/'
-visualize_threshold = 0.1
+visualize_threshold = 0.5
 
 # train_dir = '/Users/aparna/Downloads/shapenetplain_v1/train/02958343'
 # test_dir = '/Users/aparna/Downloads/shapenetplain_v1/val/02958343'
 # train_dir = '/Users/annawei/Tech/IVL/shapenetplain_v1/train/test'
 # test_dir = '/Users/annawei/Tech/IVL/shapenetplain_v1/train/test'
 # # On CCV
-train_dir = '/gpfs/data/ssrinath/shapenetplain_v1/train/02958343'
-test_dir = '/gpfs/data/ssrinath/shapenetplain_v1/val/02958343'
+train_dir = '/mnt/c/starter/shapenetplain_v1/train/02958343'
+test_dir = '/mnt/c/starter/shapenetplain_v1/val/02958343'
