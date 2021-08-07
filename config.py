5671f4999f4f93f2c16bdec3aa85d92af1b38a50
Sijie Ding <sding13@desktop3.oscar.ccv.brown.edu>
2021-08-06 23:56:35 -0400

    bug fixed and add several features

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-resolution = 16 # resolution of voxel grid binvox files and voxel centers
+resolution = 32 # resolution of voxel grid binvox files and voxel centers
 instance_dir = 'car_instance' # "shapenetplain_v1/train/02958343/1a0bc9ab92c915167ae33d942430658c"
-batch_size = 8
+batch_size = 1
 learning_rate = 1e-4
 save_dir = './saved/'
 visualize_threshold = 0.5
@@ -11,10 +11,10 @@ visualize_threshold = 0.5
 # test_dir = '/Users/annawei/Tech/IVL/shapenetplain_v1/train/test'
 # # On CCV
 
-train_dir = '/mnt/c/starter/shapenetplain_v1/train/02958343'
-test_dir = '/mnt/c/starter/shapenetplain_v1/val/02958343'
+# train_dir = '/mnt/c/starter/shapenetplain_v1/train/02958343'
+# test_dir = '/mnt/c/starter/shapenetplain_v1/val/02958343'
 
-# train_dir = '/gpfs/data/ssrinath/shapenetplain_v1/train/02958343/6c6254a92c485787f1ca7626ddabf47'
-# test_dir = '/gpfs/data/ssrinath/shapenetplain_v1/train/02958343/6c6254a92c485787f1ca7626ddabf47'
-# is_single_instance = True
+train_dir = '/gpfs/data/ssrinath/shapenetplain_v1/train/02958343'
+test_dir = '/gpfs/data/ssrinath/shapenetplain_v1/val/02958343'
+is_single_instance = False
 
--- run_ccv.sh
+++ run_ccv.sh
@@ -8,8 +8,9 @@
 module load cuda
 
 source /gpfs/runtime/opt/anaconda/2020.02/etc/profile.d/conda.sh
-conda activate deep-visual-hull
-# cd /gpfs/data/ssrinath/qwei3/deep-visual-hull/
-python run.py --load_ckpt_dir /gpfs/data/ssrinath/anatara7/deep-visual-hull/checkpoints/1624836375/ --num_epoches 200 --save_dir /gpfs/data/ssrinath/anatara7/deep-visual-hull/checkpoints/
+conda activate pt
+# cd /gpfs/data/ssrinath/sding13/deep-visual-hull/
+# python run.py --load_vgg --num_epoches 200 --save_dir /gpfs/sding13/scratch/checkpoints/
 
 
+python /users/sding13/data/sding13/deep-visual-hull/run.py --num_epoches 30 --save_dir /users/sding13/scratch/checkpoints
